a8a36513261f4e9dc14bdad47bfc49532aa02606
Eva Kktsidou <koktsidou.e@gmail.com>
2017-10-18 17:00:29 +0200

    Exarcises 1 and 2

--- Structures/src/AssignGrade.java
+++ Structures/src/AssignGrade.java
@@ -5,8 +5,15 @@ import java.util.Scanner;
 public class AssignGrade {
 	
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
 		
+		int score = 80;
+		double grade = ((100 - score)/50.0) * 3 + 1;
+		
+		 if (grade <= 4.0) {
+			System.out.println("Pass with grade: " + grade);
+		} else {
+			System.out.println("Fail with grade: " + grade);
+		} 
 		//todo: when executing the program you will be ask to write a score.
 		//The program converts the score then into a grade and prints that grade (1.0, 1.3, 1.7, ...)
 		//Note that the student passed when the student gets a score of 50.0 points (4.0)
--- Structures/src/EvenOdd.java
+++ Structures/src/EvenOdd.java
@@ -7,6 +7,15 @@ public class EvenOdd {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
+		int number = 15;
+		
+		for (int count = 0; count < number; count ++) {
+			if (count % 2 != 0) {
+				System.out.println(count);
+			}
+		}
+		
+		
 		//todo: The program takes a number as input.
 		//It then goes through all integer values till reaching that value and prints all the odd ones.
 		
