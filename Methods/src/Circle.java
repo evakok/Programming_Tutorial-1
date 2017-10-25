abc16227f02e9b08fc043b5c6951165d3280fe42
EvaKoktsidou <koktsidou.e@gmail.com>
2017-10-25 15:37:17 +0200

    Exercise 3.1

--- Methods/src/Circle.java
+++ Methods/src/Circle.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
 
 public class Circle {
 	//todo: add some methods to compute a circle's diameter, circumference and area.
 	//print some outputs to confirm you methods are working right.
+	public static double diameter(double r) {
+		double d = 2 * r;
+		return  d;
+	}
+	
+	public static double circ(double r) {
+		double pi = Math.PI ;
+		double c = 2 * pi * r ;
+		return c ;
+	}
+	
+	public static double area(double r) {
+		double pi = Math.PI ;
+		double a = pi * r * r ;
+		return a ;
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Radius: ");
+		Double r = scan.nextDouble();
+		
+		System.out.println(diameter(r));
+		System.out.println(circ(r));
+		System.out.println(area(r));
+	}
 }
