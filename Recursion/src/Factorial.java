c662332fe4ae067b256167b1fac91abd362362e9
EvaKoktsidou <koktsidou.e@gmail.com>
2017-10-26 12:24:02 +0200

    Exercises 4.1 and 4.2

--- Recursion/src/Factorial.java
+++ Recursion/src/Factorial.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
 
 public class Factorial {
 	//todo: implement a recursive functions whicht gives you the factorial value of a given number
+	public static int factorial(int x) {
+		if (x <= 1) {
+			return 1;
+		} else {
+			return x * factorial(x -1);
+			
+		}
+	}
+
+	public static void main (String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int x = scan.nextInt(); 
+		
+		System.out.println("The factorial of number " + x + " is: " + factorial(x));
+	}
 }
+
--- Recursion/src/Fibonacci.java
+++ Recursion/src/Fibonacci.java
@@ -1,5 +1,21 @@
-
+import java.util.Scanner;
 public class Fibonacci {
 	//todo: implement a recursive function which takes an integer value as inputs and outputs the corresponding Fibonacci number.
 	//if you don't know what Fibonacci numbers are you can read it here: https://en.wikipedia.org/wiki/Fibonacci_number
+public static int placeFibonacci(int n) {
+	if (n == 0 || n == 1) {
+		return n;
+	} else {
+		return placeFibonacci(n - 2) + placeFibonacci(n - 1);
+	}
 }
+
+public static void main (String[] args) {
+	Scanner scan = new Scanner(System.in);
+	int n = scan.nextInt();
+	
+	System.out.println("At the place" + n + " of the Fibonacci list, is the number: " + placeFibonacci(n));
+}
+
+}
+
